dda3947c4ba1887f959c127a737636426b51d01b
Dino Mandic <dinomandic@hotmail.com>
2017-10-15 18:56:12 -0500

    First step

--- src/App.js
+++ src/App.js
@@ -9,7 +9,17 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
+  constructor() {
+    super()
 
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    }
+  }
+  
   // updateColor
 
   // updateSize
